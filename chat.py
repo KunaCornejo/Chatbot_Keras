3d1ad8429a3c30f08fbcb0bed26dd7e54e870392
KunaCornejo <andrescornejog@me.com>
2023-04-29 21:29:10 -0400

    update from

--- chat.py
+++ chat.py
@@ -1,13 +1,11 @@
 import json 
 import numpy as np
 from tensorflow import keras
-from sklearn.preprocessing import LabelEncoder
 
 import colorama 
 colorama.init()
-from colorama import Fore, Style, Back
+from colorama import Fore, Style
 
-import random
 import pickle
 
 with open("intents.json") as file:
@@ -35,15 +33,20 @@ def chat():
         if inp.lower() == "quit":
             break
 
-        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
-                                             truncating='post', maxlen=max_len))
+        result = model\
+                 .predict(keras\
+                 .preprocessing\
+                 .sequence\
+                 .pad_sequences(tokenizer.texts_to_sequences([inp]), 
+                                truncating='post', maxlen=max_len))
         tag = lbl_encoder.inverse_transform([np.argmax(result)])
 
         for i in data['intents']:
             if i['tag'] == tag:
-                print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+                print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL, 
+                      np.random.choice(i['responses']))
 
-        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
 
-print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + Style.RESET_ALL)
+print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + \
+      Style.RESET_ALL)
 chat()
